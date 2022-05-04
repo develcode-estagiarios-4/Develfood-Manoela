902dce22c2cea10e30fe20e28c08f6232e5fe551
Manoela Pereira <manoela.pereira@develcode.com.br>
2022-05-04 16:25:40 -0300

    estilização menu navbar

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,6 @@
 import { IContainer } from "../../interface/IContainer";
 import Navbar from "../Navbar";
+import style from "./style.module.scss";
 
 export default function Container({ children }: IContainer) {
   return (
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,5 @@
+import * as FaIcons from "react-icons/fa";
+import * as MdIcons from "react-icons/md";
 import { Link } from "react-router-dom";
 
 import style from "./style.module.scss";
@@ -5,21 +7,36 @@ import style from "./style.module.scss";
 function Navbar() {
   return (
     <nav className={style.navbar}>
-      <ul className={style.navbarOptions}>
+      <ul>
         <li>
-          <Link to="/home">Home</Link>
+          <Link to="/home" className={style.navbarMenu}>
+            <MdIcons.MdHome className={style.navbarIcons} />
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/perfil">Perfil</Link>
+          <Link to="/perfil" className={style.navbarMenu}>
+            <FaIcons.FaUserCircle className={style.navbarIcons} />
+            Perfil
+          </Link>
         </li>
         <li>
-          <Link to="/menu">Menu</Link>
+          <Link to="/menu" className={style.navbarMenu}>
+            <MdIcons.MdRestaurant className={style.navbarIcons} />
+            Menu
+          </Link>
         </li>
         <li>
-          <Link to="/pedidos">Pedidos</Link>
+          <Link to="/pedidos" className={style.navbarMenu}>
+            <MdIcons.MdPhone className={style.navbarIcons} />
+            Pedidos
+          </Link>
         </li>
         <li>
-          <Link to="/promocoes">Promoções</Link>
+          <Link to="/promocoes" className={style.navbarMenu}>
+            <MdIcons.MdOutlineMoneyOffCsred className={style.navbarIcons} />
+            Promoções
+          </Link>
         </li>
       </ul>
     </nav>
